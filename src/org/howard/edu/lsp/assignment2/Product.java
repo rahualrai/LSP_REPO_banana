90746df3edb3923d788853a81186f5062832751b
Rahual <rairahual7@gmail.com>
2025-02-03 17:01:31 -0500

    Added product object.

--- src/org/howard/edu/lsp/assignment2/Product.java
+++ src/org/howard/edu/lsp/assignment2/Product.java
@@ -4,4 +4,15 @@
 package src.org.howard.edu.lsp.assignment2;
 
 public class Product {
+    int id;
+    String name;
+    double price;
+    String category;
+    
+    public Product(int id, String name, double price, String category) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
 }
