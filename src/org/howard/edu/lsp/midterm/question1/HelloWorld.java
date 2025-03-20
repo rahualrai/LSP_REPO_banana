9039e2ca07fe498d3495fc7729b3230692152999
Rahual <rairahual7@gmail.com>
2025-03-20 18:45:10 -0400

    Testing for midterm 1

---
+++ src/org/howard/edu/lsp/midterm/question1/HelloWorld.java
@@ -0,0 +1,8 @@
+package org.howard.edu.lsp.midterm.question1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World! Midterm Exam for LSP");
+        System.out.println("This is a test for the midterm exam.");
+    }
+}
