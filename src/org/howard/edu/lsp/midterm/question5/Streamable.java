87e3c7e9f27f9e7ddb7291e03072c26e88bc0b43
Rahual <rairahual7@gmail.com>
2025-03-20 20:08:42 -0400

    Streamable interface done.

---
+++ src/org/howard/edu/lsp/midterm/question5/Streamable.java
@@ -0,0 +1,10 @@
+package org.howard.edu.lsp.midterm.question5;
+
+/**
+ * The Streamable interface defines common behaviors for all streamable media types.
+ */
+public interface Streamable {
+    void play();
+    void pause();
+    void stop();
+}
